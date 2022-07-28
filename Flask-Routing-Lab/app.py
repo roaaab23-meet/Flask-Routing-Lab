9b2eefef003c353dbd1d72b35af06cbc14e3aabe
roaaab23-meet <roaa.ab86@gmail.com>
2022-07-28 16:24:52 +0300

    flask routing finished

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,16 +1,24 @@
 from flask import Flask, redirect, request, render_template, url_for
 
 
-app = Flask(  # Create a flask app
+app = Flask( # Create a flask app
     __name__,
     template_folder='templates',  # Name of html file folder
     static_folder='static'  # Name of directory for static files
 )
 
+
 # Your code should be below
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-
-
+@app.route('/product')
+def product():
+    return render_template('product.html')
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
 
 # Your code should be above
 
